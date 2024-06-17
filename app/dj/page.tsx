6be395cdc6b480c1a09defff12c05cdce8584ca5
Saketh Are <saketh.are@gmail.com>
2024-06-17 01:31:28 -0400

    spacing

--- app/dj/page.tsx
+++ app/dj/page.tsx
@@ -8,13 +8,15 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
-     <div>
      <iframe width="560" height="315" src="https://www.youtube.com/embed/fF8iStlHOrM?si=rm0dOD1Yu7pLP-hd" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+     <br/>
      <iframe width="560" height="315" src="https://www.youtube.com/embed/ELHXHFgej_4?si=nsP6MVVtzQzeaY99" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+     <br/>
      <iframe width="560" height="315" src="https://www.youtube.com/embed/UlqyFAffwsQ?si=R72M21MAsgwgrO09" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+     <br/>
      <iframe width="560" height="315" src="https://www.youtube.com/embed/sYBWjyWKP1Y?si=-FV2vRYCbKzGhvAz" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+     <br/>
      <iframe width="560" height="315" src="https://www.youtube.com/embed/bhCivp7EzXY?si=zxtQr53K3hqbguQZ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-     </div>
     </section>
   )
 }
