5e422c69b04bb16dba6618790e749315b7a074c4
Saketh Are <saketh.are@gmail.com>
2024-06-16 22:48:29 -0400

    describe car

--- app/car/page.tsx
+++ app/car/page.tsx
@@ -8,6 +8,9 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
+        <p className="mb-4">
+            {`This is my sedan. It is tasteful and practical. I like the shape.`}
+        </p>
         <img src="https://i.imgur.com/VzO984j.png"></img>
     </section>
   )
