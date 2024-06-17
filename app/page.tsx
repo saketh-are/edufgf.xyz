ac71b4c8b3b68efa4eb9bd0d9d9c4deee20dd88a
Saketh Are <saketh.are@gmail.com>
2024-06-16 22:34:06 -0400

    init homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,11 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        dudu
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`Pizza enthusiast and burger advocate. Proud father to 3 shits.`}
       </p>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
     </section>
   )
 }
