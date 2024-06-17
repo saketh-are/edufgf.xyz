6b28a08fd11605824a1bbc40a06f650af0599d78
Saketh Are <saketh.are@gmail.com>
2024-06-17 01:20:39 -0400

    fix

--- app/dj/page.tsx
+++ app/dj/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
   return (
     <section>
      <div>
-     <iframe width="560" height="315" src="https://www.youtube.com/embed/fF8iStlHOrM?si=rm0dOD1Yu7pLP-hd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/fF8iStlHOrM?si=rm0dOD1Yu7pLP-hd" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
      </div>
     </section>
   )
