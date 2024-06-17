fcbfd833082ff5d13862ea57cde463658ebd6331
Saketh Are <saketh.are@gmail.com>
2024-06-17 01:07:09 -0400

    add video

--- app/dj/page.tsx
+++ app/dj/page.tsx
@@ -8,6 +8,7 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
+     <iframe width="560" height="315" src="https://www.youtube.com/embed/fF8iStlHOrM?si=rm0dOD1Yu7pLP-hd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </section>
   )
 }
