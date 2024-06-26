1f812dd8d86dd18b213b8aa1bdd6f19669821b8b
Saketh Are <saketh.are@gmail.com>
2024-06-26 17:16:04 -0400

    add homepage img

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,9 @@ import Bounce from './bounce/bounce';
 export default function Page() {
   return (
     <section>
+      <img src="https://i.imgur.com/3hrXJio.png" width="100%" center />
+      <br/>
+
       <h1 className="mb-8 text-3xl font-semibold tracking-tighter">
         eduardo felipe gama ferreira
       </h1>
